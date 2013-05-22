139417f09f10316812cc861ac0112c54063eee82
cloudsben <cloudsben@gmail.com>
2013-05-22 14:06:20 +0800

    update some api test

--- application/controllers/speakerapi_test.php
+++ application/controllers/speakerapi_test.php
@@ -30,39 +30,38 @@ class Speakerapi_test extends CI_Controller
 	{
 		$this->rest->initialize(array('server' => $this->server));
 		$ret = $this->rest->get('speakerapi/all_category/');
-		$this->unit->run($ret['item'], 'is_array', '测试所有分类返回结果!');
-		// var_dump($ret);
+		$this->unit->run(count($ret['item']) > 0, TRUE, '测试所有分类返回结果!');
 	}
+
 	function test_slides_get()
 	{
 		$this->rest->initialize(array('server' => $this->server));
-		$ret = $this->rest->get('/speakerapi/slides/books/3');
-		$this->unit->run($ret['slides'], 'is_object', '测试某一个分类的列表返回结果!');
-		// var_dump($ret);
+		$ret = $this->rest->get('speakerapi/slides/books/3');
+		$this->unit->run(count($ret['slides']) > 0, TRUE, '测试某一个分类的列表返回结果!');
 	}
 	
 	function test_detail_get()
 	{
 		$this->rest->initialize(array('server' => $this->server));
 		$ret = $this->rest->get('speakerapi/detail?url=https://speakerdeck.com/geeforr/whats-new-in-ruby-2-dot-0');
-		$this->unit->run($ret['data_slides'], 'is_object', '测试内容页面的slides返回结果!');
-		// var_dump($ret);
+		$this->unit->run($ret['data_id'], 'is_string', '测试 内容 id 返回结果!');
+		$this->unit->run($ret['data_download_pdf'], 'is_string', '测试 内容 pdf 下载地址 返回结果!');
+		$this->unit->run(count($ret['data_slides']) > 0, TRUE, '测试内容的slides返回结果!');
 	}
 	
 	function test_search_get()
 	{
 		$this->rest->initialize(array('server' => $this->server));
 		$ret = $this->rest->get('speakerapi/search/books/2');
-		$this->unit->run($ret['slides'], 'is_object', '测试搜索列表返回结果!');
-		// var_dump($ret);
+		$this->unit->run(count($ret['slides']) > 0, TRUE, '测试搜索列表返回结果!');
 	}
 	
 	function test_author_get()
 	{
 		$this->rest->initialize(array('server' => $this->server));
 		$ret = $this->rest->get('speakerapi/author/cloudsben');
-		// var_dump($ret);
-		$this->unit->run($ret['author_slides'], 'is_object', '测试作者页面 slides 返回结果');
+		$this->unit->run(strlen($ret['author_name']) > 0, TRUE, '测试作者名称返回结果');
+		$this->unit->run(strlen($ret['author_url']) > 0, TRUE, '测试作者 url 地址返回结果');
 	}
 	
 }
